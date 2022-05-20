8004366063b2c3aab99a240ccbc4061a0e513e23
Daniel Carlström Schad <da8454sh-s@student.lu.se>
2022-05-20 11:46:44 +0200

    Update

--- Al version/Final project.py
+++ Al version/Final project.py
@@ -16,19 +16,19 @@ x=np.array([1,0])
 #print(fcn(x))
 test.plot(100,(-10,10,-10,10))
 
-#%%
-def fca(x:np.array):
-    x1,x2=x[0],x[1]
-    f1=x1**3- 3*x1*x2**2 - 2*x1 -2
-    f2= 3* x1**2 *x2 - x2**3 - 2*x2
-    return np.array([f1,f2])
-test2=fp.Fractal2D(fca)
-test2.plot(200,(-2,2,-2,2))
-
-#%%
-def fcb(x:np.array):
-    x1,x2=x[0],x[1]
-    f1= x1**8- 28
-    f2= 3* x1**2 *x2 - x2**3 - 2*x2
-    return np.array([f1,f2])
+# #%%
+# def fca(x:np.array):
+#     x1,x2=x[0],x[1]
+#     f1=x1**3- 3*x1*x2**2 - 2*x1 -2
+#     f2= 3* x1**2 *x2 - x2**3 - 2*x2
+#     return np.array([f1,f2])
+# test2=fp.Fractal2D(fca)
+# test2.plot(200,(-2,2,-2,2))
+#
+# #%%
+# def fcb(x:np.array):
+#     x1,x2=x[0],x[1]
+#     f1= x1**8- 28
+#     f2= 3* x1**2 *x2 - x2**3 - 2*x2
+#     return np.array([f1,f2])
 
