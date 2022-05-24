b707b6064d831985b10df317299ed05937794078
Daniel Carlström Schad <da8454sh-s@student.lu.se>
2022-05-24 13:46:17 +0200

    Removed old files

--- Al version/test_fp_aux.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 18 16:10:29 2022
-
-@author: Anna-Lisa Rathsman
-"""
-
--- FinalProject/__init__.py
+++ FinalProject/__init__.py
@@ -1,2 +1,1 @@
 from .fractal2d import Fractal2D
-from .fractal2d_old import Fractal2D as Fractal2D_old
